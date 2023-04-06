efb274d67dc4f9969b37dbc60be94553fb1e3559
antonov <artem@antonov.es>
2023-04-07 00:15:33 +0200

    Added Categories navigation.

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,6 +7,7 @@ const Logo = () => {
     const router = useRouter();
     return (
         <Image
+            onClick={() => router.push('/')}
             alt='SalouBnB Logo'
             className='hidden md:block cursor-pointer'
             height={100}
