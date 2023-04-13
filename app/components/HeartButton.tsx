9b01e276bf513df8957130fc940ec3259cc03b12
antonov <artem@antonov.es>
2023-04-13 16:33:20 +0200

    Add a property to favorite

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,16 +2,14 @@
 
 import {SafeUser} from "@/app/types";
 import {AiFillHeart, AiOutlineHeart} from "react-icons/ai";
+import useFavorite from "@/app/hooks/useFavorite";
 
 interface HeartButtonProps {
   listingId: string;
   currentUser?: SafeUser | null
 }
 const HeartButton: React.FC<HeartButtonProps> = ({listingId, currentUser}) => {
-  const hasFavorited = false;
-  const toggleFavorite = () => {
-
-  }
+  const {hasFavorited, toggleFavorite} = useFavorite({listingId, currentUser});
   return (
     <div onClick={toggleFavorite} className="relative hover:opacity-80 transition cursor-pointer">
       <AiOutlineHeart className="absolute fill-white -top-[2px] -right-[2px]" size={28}/>
