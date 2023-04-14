2c6c5f0faec32e0d29aa5683f0b85523775628fe
antonov <artem@antonov.es>
2023-04-14 14:43:23 +0200

    Listing details page

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
     <ClientOnly>
       <Container>
         <div className="
-          pt-48
+          pt-24
           grid
           grid-cols-1
           sm:grid-cols-2
